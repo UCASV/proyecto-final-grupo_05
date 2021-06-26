2f7d8b404f349ab87c1201ef3042e2b0e50d37d1
PaoloDuenas <00230720@uca.edu.sv>
2021-06-26 02:14:43 -0600

    design updt 1.0

--- Proyecto/Proyecto/View/frmSecondDose.Designer.cs
+++ Proyecto/Proyecto/View/frmSecondDose.Designer.cs
@@ -29,21 +29,88 @@ namespace Proyecto.View
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(75, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 29);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVerify.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnVerify.Location = new System.Drawing.Point(291, 248);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(201, 61);
+            this.btnVerify.TabIndex = 1;
+            this.btnVerify.Text = "Verificar";
+            this.btnVerify.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(16, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "DUI:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(247, 174);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(289, 58);
+            this.panel1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(210, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(363, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingrese DUI para verificar su segunda dosis:";
             // 
             // frmSecondDose
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 474);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnVerify);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmSecondDose";
             this.Text = "frmSecondDose";
             this.Load += new System.EventHandler(this.frmSecondDose_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnVerify;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Proyecto/Proyecto/View/frmSecondDose.cs
+++ Proyecto/Proyecto/View/frmSecondDose.cs
@@ -35,5 +35,7 @@ namespace Proyecto.View
                 }
             }
         }
+
+       
     }
 }
