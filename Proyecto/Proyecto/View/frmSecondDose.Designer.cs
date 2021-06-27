f41d7bacbaa444451b3615752c7e94c0fbef0c71
PaoloDuenas <00230720@uca.edu.sv>
2021-06-26 18:03:49 -0600

    SecondDose almost ready

--- Proyecto/Proyecto/View/frmSecondDose.Designer.cs
+++ Proyecto/Proyecto/View/frmSecondDose.Designer.cs
@@ -29,32 +29,34 @@ namespace Proyecto.View
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.btnVerify = new System.Windows.Forms.Button();
+            this.txtScndDse_dui = new System.Windows.Forms.TextBox();
+            this.btnScndDse_Verify = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
+            this.lblScndDse_Info = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtScndDse_dui
             // 
-            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(75, 14);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(201, 29);
-            this.textBox1.TabIndex = 0;
+            this.txtScndDse_dui.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtScndDse_dui.Location = new System.Drawing.Point(75, 14);
+            this.txtScndDse_dui.Name = "txtScndDse_dui";
+            this.txtScndDse_dui.Size = new System.Drawing.Size(201, 29);
+            this.txtScndDse_dui.TabIndex = 0;
             // 
-            // btnVerify
+            // btnScndDse_Verify
             // 
-            this.btnVerify.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnVerify.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnVerify.Location = new System.Drawing.Point(291, 248);
-            this.btnVerify.Name = "btnVerify";
-            this.btnVerify.Size = new System.Drawing.Size(201, 61);
-            this.btnVerify.TabIndex = 1;
-            this.btnVerify.Text = "Verificar";
-            this.btnVerify.UseVisualStyleBackColor = true;
+            this.btnScndDse_Verify.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnScndDse_Verify.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnScndDse_Verify.Location = new System.Drawing.Point(55, 162);
+            this.btnScndDse_Verify.Name = "btnScndDse_Verify";
+            this.btnScndDse_Verify.Size = new System.Drawing.Size(140, 61);
+            this.btnScndDse_Verify.TabIndex = 1;
+            this.btnScndDse_Verify.Text = "Verificar";
+            this.btnScndDse_Verify.UseVisualStyleBackColor = true;
+            this.btnScndDse_Verify.Click += new System.EventHandler(this.btnScndDse_Verify_Click);
             // 
             // label1
             // 
@@ -70,8 +72,8 @@ namespace Proyecto.View
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Location = new System.Drawing.Point(247, 174);
+            this.panel1.Controls.Add(this.txtScndDse_dui);
+            this.panel1.Location = new System.Drawing.Point(55, 88);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(289, 58);
             this.panel1.TabIndex = 3;
@@ -80,20 +82,30 @@ namespace Proyecto.View
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(210, 130);
+            this.label2.Location = new System.Drawing.Point(55, 44);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(363, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "Ingrese DUI para verificar su segunda dosis:";
+            // 
+            // lblScndDse_Info
+            // 
+            this.lblScndDse_Info.AutoSize = true;
+            this.lblScndDse_Info.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblScndDse_Info.Location = new System.Drawing.Point(55, 292);
+            this.lblScndDse_Info.Name = "lblScndDse_Info";
+            this.lblScndDse_Info.Size = new System.Drawing.Size(0, 25);
+            this.lblScndDse_Info.TabIndex = 7;
             // 
             // frmSecondDose
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 474);
+            this.Controls.Add(this.lblScndDse_Info);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.btnVerify);
+            this.Controls.Add(this.btnScndDse_Verify);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmSecondDose";
             this.Text = "frmSecondDose";
@@ -107,10 +119,11 @@ namespace Proyecto.View
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button btnVerify;
+        private System.Windows.Forms.TextBox txtScndDse_dui;
+        private System.Windows.Forms.Button btnScndDse_Verify;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblScndDse_Info;
     }
 }
--- Proyecto/Proyecto/View/frmSecondDose.cs
+++ Proyecto/Proyecto/View/frmSecondDose.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using iTextSharp.text;
+using Proyecto.Covid19_Context;
 
 namespace Proyecto.View
 {
@@ -36,6 +38,71 @@ namespace Proyecto.View
             }
         }
 
-       
+        private void btnScndDse_Verify_Click(object sender, EventArgs e)
+        {
+            bool verifcation = txtScndDse_dui.Text.Length == 10;
+
+            //Encontrar si el DUI existe y validar si es su segunda dosis
+            using (var db = new COVID19_DATABASEContext())
+            {
+                List<Citum> cita = db.Cita.ToList();
+                List<Citum> identifyCita = cita.Where(c => c.IdentificadorCita == 1).ToList();
+                List<Ciudadano> citizens = db.Ciudadanos.ToList();
+                List<Ciudadano> exist = citizens.Where(u => u.Dui == txtScndDse_dui.Text).ToList();
+                if (exist.Count() > 0 && identifyCita.Count() > 0)
+                {
+                    if (verifcation)
+                    {
+                        DateTime newDateCitum = Convert.ToDateTime("2020-01-01 13:00");
+                        string duivalidatorio = txtScndDse_dui.Text;
+                        int idCita;
+                        //obtenemos la fecha de la primera cita correspondiente al dui
+                        foreach (var element in cita)
+                        {
+                            if (duivalidatorio == element.IdCiudadano)
+                            {
+                                newDateCitum = element.FechaHora;
+                            }
+                        }
+                        //agregamos los dias para la proxima cita
+                        newDateCitum = newDateCitum.AddDays(45);
+
+                        //randomizador para seleccionar lugar donde sera la proxima cita y el empleado que atiende la cita
+                        Random rdm = new Random();
+                        int a = rdm.Next(1, 5);
+                        int b = rdm.Next(1, 10);
+
+
+                        
+                        Citum unCitum = new Citum();
+                        {
+                            unCitum.FechaHora = newDateCitum;
+                            unCitum.IdentificadorCita = 2;
+                            unCitum.IdLugar = a;
+                            unCitum.IdentificadorEmpleado = b;
+                            unCitum.IdCiudadano = txtScndDse_dui.Text;
+                        };
+
+                        db.Add(unCitum);
+                        db.SaveChanges();
+
+                        //Mesagge box para mostrar los datos de la proxima cita
+                        MessageBox.Show("Su cita para la segunda dosis sera el " + newDateCitum, "Covid-19 Vacunación",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Datos Inválidos, verifique el ingreso de los datos", "Covid-19 Vacunación",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El DUI no existe o su segunda dosis ya ha sido agendada", "Covid-19 Vacunación",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        
+        }
     }
 }
