3f9711ce22be8af2622e8feba70ac38f34ae1734
PaoloDuenas <00230720@uca.edu.sv>
2021-06-26 19:47:50 -0600

    Second Dose Ready

--- Proyecto/Proyecto/View/frmSecondDose.cs
+++ Proyecto/Proyecto/View/frmSecondDose.cs
@@ -43,13 +43,15 @@ namespace Proyecto.View
             bool verifcation = txtScndDse_dui.Text.Length == 10;
 
             //Encontrar si el DUI existe y validar si es su segunda dosis
+            // Tambien restringir el maximo de citas agendadas
             using (var db = new COVID19_DATABASEContext())
             {
                 List<Citum> cita = db.Cita.ToList();
                 List<Citum> identifyCita = cita.Where(c => c.IdentificadorCita == 1).ToList();
+                List<Citum> restrict = cita.Where(c => c.IdCiudadano == txtScndDse_dui.Text).ToList();
                 List<Ciudadano> citizens = db.Ciudadanos.ToList();
                 List<Ciudadano> exist = citizens.Where(u => u.Dui == txtScndDse_dui.Text).ToList();
-                if (exist.Count() > 0 && identifyCita.Count() > 0)
+                if (exist.Count() > 0 && identifyCita.Count() > 0 && restrict.Count() < 2)
                 {
                     if (verifcation)
                     {
